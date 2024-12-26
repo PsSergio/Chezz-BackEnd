e81f21a45a896dc2114761d784f5c13f7a7b3db6
Paulo Sergio <pscosta2008@gmail.com>
2024-12-26 15:58:50 -0300

    feat: updating db scrpit

--- database.sql
+++ database.sql
@@ -1,9 +1,11 @@
 create database chezz
 use chezz
 
+drop database chezz
+
 create table Player (
 
-	id bigint primary key,
+	id bigint primary key AUTO_INCREMENT,
 	email varchar(50) not null,
 	password varchar(50) not null,
 	username varchar(30) not null
@@ -12,10 +14,34 @@ create table Player (
 
 create table MatchGame (
 
-	id bigint primary key,
+	id bigint primary key AUTO_INCREMENT,
 	owner_id bigint not null,
-	guest_id bigint not null,
+	guest_id bigint,
 	foreign key (owner_id) references Player(id),
 	foreign key (guest_id) references Player(id)
 
 )
+
+create table SessionPlayer (
+
+	id bigint primary key auto_increment,
+	playerId bigint not null,
+	init_date Date not null,
+	final_date Date not null,
+	foreign key (playerId) references Player(id)
+
+)
+
+select * from Player
+
+-- Inserindo o primeiro jogador (Owner)
+INSERT INTO Player (id, email, password, username)
+VALUES (1, 'owner@example.com', 'password123', 'OwnerPlayer');
+
+-- Inserindo o segundo jogador (Guest)
+INSERT INTO Player (id, email, password, username)
+VALUES (2, 'guest@example.com', 'password456', 'GuestPlayer');
+
+-- Inserindo uma partida com o primeiro jogador como Owner e o segundo jogador como Guest
+INSERT INTO MatchGame (id, owner_id, guest_id)
+VALUES (1, 1, 2);
